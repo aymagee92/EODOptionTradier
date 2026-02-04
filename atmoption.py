80e5d679704027f7768e7a68e7395cb3a3b7ae4e
aymagee92 <89717158+aymagee92@users.noreply.github.com>
2026-02-04 02:32:41 -0500

    Update atmoption.py

--- atmoption.py
+++ atmoption.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template_string
 
 app = Flask(__name__)
 
-@app.route("/atmoption")
+@app.route("/atmoptions")
 def atmoption():
     return render_template_string("""
 <!doctype html>
